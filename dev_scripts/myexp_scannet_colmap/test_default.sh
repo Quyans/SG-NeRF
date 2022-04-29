1e44d978e764526881a0268d095ae19af2cbdcad
novasky0709 <wangyuze19980709@163.com>
2022-04-29 16:32:28 +0800

    Add test_default.sh

--- dev_scripts/myexp_scannet_colmap/test_default.sh
+++ dev_scripts/myexp_scannet_colmap/test_default.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-novel_cam_trajectory="1"
+
 nrCheckpoint="../checkpoints"
 nrDataRoot="../data_src"
-name='11-scene000-edit-center_sofa_trans'
-resume_iter="latest" #latest
+name='00-t'
+
+resume_iter=200000 # 20000 #latest
 data_root="${nrDataRoot}/scannet/scans/"
-scan="scene0000_00" # camera view i want to rendering
+scan="scene0113_00"
 normview=0
 edge_filter=10 # pixels crop out at image edge
+
 point_conf_mode="1" # 0 for only at features, 1 for multi at weight
 point_dir_mode="1" # 0 for only at features, 1 for color branch
 point_color_mode="1" # 0 for only at features, 1 for color branch
+
 agg_feat_xyz_mode="None"
 agg_alpha_xyz_mode="None"
 agg_color_xyz_mode="None"
@@ -29,13 +32,17 @@ max_o=610000
 ranges=" -10.0 -10.0 -10.0 10.0 10.0 10.0 "
 SR=24
 K=8
-P=30
+P=26
 NN=2
+
 
 act_type="LeakyReLU"
 
 agg_intrp_order=2
 agg_distance_kernel="linear" #"avg" #"feat_intrp"
+weight_xyz_freq=2
+weight_feat_dim=8
+
 point_features_dim=32
 shpnt_jitter="passfunc" #"uniform" # uniform gaussian
 
@@ -44,7 +51,8 @@ apply_pnt_mask=1
 shading_feature_mlp_layer0=1 #2
 shading_feature_mlp_layer1=2 #2
 shading_feature_mlp_layer2=0 #1
-shading_feature_mlp_layer3=2 #1
+shading_feature_mlp_layer3=0 #1
+shading_feature_mlp_layer4=1 #1
 shading_alpha_mlp_layer=1
 shading_color_mlp_layer=4
 shading_feature_num=256
@@ -60,7 +68,7 @@ model='mvs_points_volumetric'
 near_plane=0.1
 far_plane=8.0
 which_ray_generation='near_far_linear' #'nerf_near_far_linear' #
-
+domain_size='1'
 dir_norm=0
 
 which_tonemap_func="off" #"gamma" #
@@ -72,10 +80,18 @@ num_pos_freqs=10
 num_viewdir_freqs=4 #6
 
 random_sample='random'
-random_sample_size=28 # 32 * 32 = 1024
+random_sample_size=56 # 32 * 32 = 1024
+
 batch_size=1
 
+plr=0.002
+lr=0.0005 # 0.0005 #0.00015
+lr_policy="iter_exponential_decay"
+lr_decay_iters=1000000
+lr_decay_exp=0.1
+
 gpu_ids='0'
+
 checkpoints_dir="${nrCheckpoint}/scannet/"
 resume_dir="${nrCheckpoint}/init/dtu_dgt_d012_img0123_conf_agg2_32_dirclr20"
 
@@ -85,8 +101,10 @@ color_loss_weights=" 1.0 0.0 0.0 "
 color_loss_items='ray_masked_coarse_raycolor ray_miss_coarse_raycolor coarse_raycolor'
 test_color_loss_items='coarse_raycolor ray_miss_coarse_raycolor ray_masked_coarse_raycolor'
 
+
+
 bg_color="white" #"0.0,0.0,0.0,1.0,1.0,1.0"
-split="test"
+split="train"
 
 cd run
 
@@ -139,6 +157,7 @@ python3 test_ft.py \
         --shading_feature_mlp_layer1 $shading_feature_mlp_layer1 \
         --shading_feature_mlp_layer2 $shading_feature_mlp_layer2 \
         --shading_feature_mlp_layer3 $shading_feature_mlp_layer3 \
+        --shading_feature_mlp_layer4 $shading_feature_mlp_layer4 \
         --shading_feature_num $shading_feature_num \
         --dist_xyz_freq $dist_xyz_freq \
         --shpnt_jitter $shpnt_jitter \
@@ -164,7 +183,4 @@ python3 test_ft.py \
         --z_depth_dim $z_depth_dim \
         --max_o $max_o \
         --query_size $query_size \
-        --debug \
-        --novel_cam_trajectory $novel_cam_trajectory
-
-
+        --debug
